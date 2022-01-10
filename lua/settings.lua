4ba8961c297a49305d8c9338ec954db56034aebf
Rahmat Ardiansyah <rahmat21@student.uir.ac.id>
2022-01-10 13:08:00 +0700

    add indent & comment config

--- lua/settings.lua
+++ lua/settings.lua
@@ -22,10 +22,20 @@ vim.wo.colorcolumn = '80'
 -- restore cursor position
 vim.cmd([[
 autocmd BufReadPost *
-    \ if line("'\"") > 1 && line("'\"") <= line("$") |
-    \   execute "normal! g`\"" |
-    \ endif
+  \ if line("'\"") > 1 && line("'\"") <= line("$") |
+  \   execute "normal! g`\"" |
+  \ endif
 ]])
+
+-- 2 spaces for selected filetypes
+vim.cmd [[
+  autocmd FileType xml,html,xhtml,css,scss,javascript,lua,yaml setlocal shiftwidth=2 tabstop=2
+]]
+
+-- don't auto commenting new lines
+vim.cmd [[
+  au BufEnter * set fo-=c fo-=r fo-=o
+]]
 -- Search
 vim.opt.ignorecase = true
 vim.opt.hlsearch = true
